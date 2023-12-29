233eb1ac6461e0ecb5103eefafcdb2c685b01028
Abhishek <abhishekparmar162@gmail.com>
2023-12-29 17:19:25 +0530

    added fevicon

--- app/moreprojects.tsx
+++
@@ -1,8 +0,0 @@
-import { ProjectsList } from "@/components/ProjectsList";
-import ProjectItem from "../components/ProjectItem";
-
-const page = () => {
-  return <div>hey</div>;
-};
-
-export default page;
